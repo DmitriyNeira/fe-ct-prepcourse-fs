ca14480eda9c09a179000af9055b3bbef8358471
DmitriyNeira <dmitriyneira2@gmail.com>
2023-12-06 20:59:01 -0500

    Ejercicios resueltos del módulo 6

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -7,6 +7,19 @@ function crearGato(nombre, edad) {
    // La propiedad "meow" será una función que retorne el string: "Meow!".
    // Retornar el objeto.
    // Tu código:
+   var gato = {
+      nombre: nombre,
+      edad: edad
+   };
+   //* Mis errores: está bien usar el console.log en pruebas, pero te recuerdo que no devolverá al inicio
+   //* si te piden que crees el objeto, no pongas el return solo de la función, pon todo el objeto para que pueda comprobarse.
+   gato.meow = function() {
+      return "Meow!";
+   };
+
+   return gato;
+
+
 }
 
 function nuevoUsuario(nombre, email, password) {
@@ -14,6 +27,15 @@ function nuevoUsuario(nombre, email, password) {
    // Este debe tener las propiedades: "nombre", "email" y "password" con sus respectivos valores.
    // Retornar el objeto.
    // Tu código:
+   var usuario={
+
+      nombre: nombre,
+      email: email,
+      password: password
+
+   };
+
+   return usuario;
 }
 
 function agregarPropiedad(objeto, propiedad) {
@@ -22,6 +44,10 @@ function agregarPropiedad(objeto, propiedad) {
    // Esta propiedad será igual al valor `null`.
    // Retornar el objeto.
    // Tu código:
+   //* mi error: coloqué el null en [], le devolví un array con un null dentro en vez de solo null.
+   objeto[propiedad]=null;
+   return objeto;
+
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -29,43 +55,65 @@ function invocarMetodo(objeto, metodo) {
    // Esta propiedad contiene una función en su interior. Debes invocarla/ejecutarla.
    // [NOTA]: no necesitar retornar nada.
    // Tu código:
+
+   //! RECORDATORIO si le pones con notación de punto pondrá literalmente la palabra metodo
+   //! Además, si pones el valor de metodo en otra variable y pones esa variable, colocará el valor de la variable de todas formas. Usa notación de corchetes
+   objeto[metodo]();
+   
+
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
    // El parámetro "objetoMisterioso" posee una propiedad con el nombre "numeroMisterioso".
    // Debes multiplicar este número por 5 y retornar el resultado.
    // Tu código:
+
+   return (objetoMisterioso.numeroMisterioso)*5;
 }
 
 function eliminarPropiedad(objeto, propiedad) {
    // El parámetro "propiedad" es una propiedad del objeto que recibes.
    // Debes eliminarla del objeto y retornarlo finalmente.
    // Tu código:
+   delete objeto[propiedad];
+   return objeto;
 }
 
 function tieneEmail(objetoUsuario) {
    // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   //* mi error: al colocar vi que sea diferente a undefined, luego que no sea null, cuando debí poner ambar
+   // ambas hacen que no tenga un valor definido.
+   if (objetoUsuario.email !== undefined && objetoUsuario.email !== null ){
+      return true;
+   } else return false;
 }
 
 function tienePropiedad(objeto, propiedad) {
    // Verifica si el objeto recibido posee una propiedad con el mismo nombre que el parámetro "propiedad".
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   
+   return objeto.hasOwnProperty(propiedad);
 }
 
 function verificarPassword(objetoUsuario, password) {
    // Verifica si la propiedad "password" del "objetoUsuario" coincide con el parámetro "password".
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   if (objetoUsuario["password"]==password){
+      return true;
+   } else return false;
 }
 
 function actualizarPassword(objetoUsuario, nuevaPassword) {
-   // Reemplaza la contrseña guardada en la propiedad "password" del "objetoUsuario".
+   // Reemplaza la contraseña guardada en la propiedad "password" del "objetoUsuario".
    // La nueva contraseña la recibes por parámetro.
    // Retornar el objeto.
    // Tu código:
+   objetoUsuario["password"]=nuevaPassword;
+   return objetoUsuario;
 }
 
 function agregarAmigo(objetoUsuario, nuevoAmigo) {
@@ -73,6 +121,9 @@ function agregarAmigo(objetoUsuario, nuevoAmigo) {
    // Debes agregar el "nuevoAmigo" al final de este arreglo.
    // Retornar el objeto.
    // Tu código:
+   //? Puedes agregar el .push directamente al lado de amigos, lo que facilita modificar el array.
+   objetoUsuario.amigos.push(nuevoAmigo);
+   return objetoUsuario;
 }
 
 function pasarUsuarioAPremium(objetoMuchosUsuarios) {
@@ -81,6 +132,15 @@ function pasarUsuarioAPremium(objetoMuchosUsuarios) {
    // Define esta propiedad de todos los usuarios como true.
    // Retornar el arreglo.
    // Tu código:
+
+
+   //* Error: estás trabajando con un arreglo con objetos adentro, no al reves, debes usar un for común
+   for (var i = 0; i < objetoMuchosUsuarios.length; i++) {
+      //Aqui tomas el array en cada posición y le adjuntas la propiedad .esPremium, ya que, con esto accedes a la propiedad que está en todos los usuarios.
+      objetoMuchosUsuarios[i].esPremium = true;
+   }
+
+   return objetoMuchosUsuarios;
 }
 
 function sumarLikesDeUsuario(objetoUsuario) {
@@ -89,8 +149,18 @@ function sumarLikesDeUsuario(objetoUsuario) {
    // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
    // Debes sumar los likes de todos los post y retornar el resultado.
    // Tu código:
-}
 
+   //* error, no necesitas un for in dentro del for para acceder a los datos.
+   var like=0;
+
+   for (let i=0;i<objetoUsuario.posts.length;i++){
+      
+      like= like+ objetoUsuario.posts[i].likes;
+      
+
+   }
+   return like;
+}
 function agregarMetodoCalculoDescuento(objetoProducto) {
    // Agrega una propiedad al "objetoProducto" con el nombre "calcularPrecioDescuento".
    // Esta propiedad debe ser una función que multiplique el precio del producto por el porcentajeDeDescuento.
@@ -102,6 +172,13 @@ function agregarMetodoCalculoDescuento(objetoProducto) {
    // PorcentajeDeDescuento ---> 0.2
    // Precio final ---> 8
    // Tu código:
+   objetoProducto.calcularPrecioDescuento= function(){
+      Descuento=this.porcentajeDeDescuento*this.precio;
+      let Precio_Final=this.precio-Descuento;
+      return Precio_Final
+   };
+return objetoProducto;
+
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
